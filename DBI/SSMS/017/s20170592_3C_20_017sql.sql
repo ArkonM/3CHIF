e56ef99f213e7181b1130a5661ef0277155ff3bf
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2021-02-25 17:57:54 +0100

    017

---
+++ DBI/SSMS/017/s20170592_3C_20_017sql.sql
@@ -0,0 +1,50 @@
+use schulungsfirma;
+go
+
+--s20170592
+select p.pnr, p.fname
+  from person p
+  where p.pnr in (select b.pnr
+				    from besucht b
+				    join kveranst k on k.knr = b.knr
+					     		   and k.knrlfnd = b.knrlfnd
+			       where k.ort = 'Wien'
+			         and b.pnr = p.pnr
+			     )
+union
+select p.pnr, p.fname
+  from person p 
+  where p.pnr in (select pnr
+				    from kveranst
+				   where ort = 'Wien'
+				     and pnr = p.pnr
+				 )
+ order by p.pnr
+;
+go
+
+
+--20170592
+--set language German;
+--set DATEFIRST 7;
+select k.knr, k.bezeichn, k.tage, kv.knrlfnd,
+       kv.von, DATENAME(dw, DATEPART(dw, kv.von)) von_wotag, kv.bis, 
+	   DATENAME(dw, DATEPART(dw, kv.bis)) bis_wotag, DATEDIFF(dw, kv.von, kv.bis)+1 von_bis_tage
+  from kurs k
+  join kveranst kv on k.knr = kv.knr
+  where DATEPART(dw, kv.von) < DATEPART(dw, kv.bis)
+    and DATEPART(dw, kv.bis) >=6
+	 or DATEPART(dw, kv.von) > DATEPART(dw, kv.bis)
+;
+go
+
+
+--20170592
+select distinct p.pnr, p.fname, p.vname
+  from kveranst kv
+  join person p on p.pnr = kv.pnr
+  join referent ref on kv.pnr = ref.pnr
+ where DATEDIFF(yy, ref.gebdat, kv.bis) >= 60
+ order by p.pnr
+;
+go
